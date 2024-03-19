0b58b4f43e9570e0abe286385c3310fec3a602a6
germans <grajevskis@gmail.com>
2024-03-19 14:53:11 +0200

    add Project File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import './App.css';
 import './App.css';
 
+import Product from './components/Product';
+
 function App() {
+
+  const users = [
+    {
+      id: 1,
+      firstname: 'Oleg',
+      lastname: 'Petrov',
+      age: 30
+    },
+    {
+      id: 2,
+      firstname: 'Ivan',
+      lastname: 'Demidov',
+      age: 45
+    },
+    {
+      id: 3,
+      firstname: 'Olga',
+      lastname: 'Sidorova',
+      age: 14
+    },
+    {
+      id: 4,
+      firstname: 'Irina',
+      lastname: 'Sokolova',
+      age: 17
+    },
+    {
+      id: 5,
+      firstname: 'Boris',
+      lastname: 'Ushanov',
+      age: 23
+    },
+    {
+      id: 6,
+      firstname: 'Anna',
+      lastname: 'Petrova',
+      age: 46
+    }
+  ]
+
+  const products = [
+    {
+      id: 1,
+      title: 'Apple',
+      description:'pretti',
+      price: 47,
+      discount: 35 
+    },
+    {
+      id: 2,
+      title: 'Banana',
+      description:'pretti',
+      price: 47,
+      discount: 35 
+    },
+    {
+      id: 3,
+      title: 'Peach',
+      description:'pretti',
+      price: 47,
+      discount: 35 
+    },
+    {
+      id: 4,
+      title: 'Orange',
+      description:'pretti',
+      price: 47,
+      discount: 35 
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Header</h1>
+      {/* {
+        users.map(el => <User firstname={el.firstname} lastname={el.lastname} age={el.age} key={el.id} />)
+      } */}
+
+      {
+        products.map(el => <Product key={el.id} title={el.title} description={el.description} price={el.price} discount={el.discount} />)
+      }
     </div>
   );
 }
